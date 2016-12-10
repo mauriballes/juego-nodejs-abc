bf7c4b2d8de9a00c0d07f701eb991e79644b269b
Mauricio Ballesteros <mballes95@gmail.com>
2016-12-09 22:44:02 -0400

    Agregando CORS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -18,6 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 // Rutas
 app.use('/', index);
