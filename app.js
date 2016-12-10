ab3b5c751c9556e95ef439e197ffa77c5353385a
Mauricio Ballesteros <mballes95@gmail.com>
2016-12-10 03:18:59 -0400

    Quitando Favicon

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var cors = require('cors');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -12,8 +11,6 @@ var index = require('./routes/index');
 
 var app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
