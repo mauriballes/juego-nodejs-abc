e40b36307e64e05d79043cedeecd318219898406
Mauricio Ballesteros <mballes95@gmail.com>
2016-12-12 14:36:10 -0400

    Modificando el Seed

--- models/seeder.js
+++ models/seeder.js
@@ -25,18 +25,16 @@ mongoose.connect('mongodb://localhost:27017/abcgame', function () {
 function seed() {
     // Unidad
     (new Unidad({nombre: 'Saludos', descripcion: 'Unidas de Saludos', nivel: 1})).save(function (err, docUnit1) {
-        (new Palabra({letras: 'Hola', unidad: docUnit1})).save(function (err, doc) {
-            (new Palabra({letras: 'Buenas', unidad: docUnit1})).save(function (err, doc) {
-                (new Palabra({letras: 'Alo', unidad: docUnit1})).save(function (err, doc) {
-                    (new Unidad({
-                        nombre: 'Despedidas',
-                        descripcion: 'Unidas de Despedidas',
-                        nivel: 2
-                    })).save(function (err, docUnit2) {
-                        (new Palabra({letras: 'Adios', unidad: docUnit2})).save(function (err, doc) {
-                            (new Palabra({letras: 'Chau', unidad: docUnit2})).save(function (err, doc) {
-                                mongoose.disconnect();
-                            });
+        (new Palabra({letras: 'HOLA', unidad: docUnit1})).save(function (err, doc) {
+            (new Palabra({letras: 'ALO', unidad: docUnit1})).save(function (err, doc) {
+                (new Unidad({
+                    nombre: 'Despedidas',
+                    descripcion: 'Unidas de Despedidas',
+                    nivel: 2
+                })).save(function (err, docUnit2) {
+                    (new Palabra({letras: 'ADIOS', unidad: docUnit2})).save(function (err, doc) {
+                        (new Palabra({letras: 'CHAU', unidad: docUnit2})).save(function (err, doc) {
+                            mongoose.disconnect();
                         });
                     });
                 });
@@ -47,25 +45,75 @@ function seed() {
 
 function seedTest() {
     (new Unidad({nombre: 'Saludos', descripcion: 'Unidas de Saludos', nivel: 1})).save(function (err, docUnit1) {
-        (new Palabra({letras: 'Hola', unidad: docUnit1})).save(function (err, doc) {
+        (new Palabra({letras: 'HOLA', unidad: docUnit1})).save(function (err, doc) {
             (new Unidad({nombre: 'Comida', descripcion: 'Unidas de Comida', nivel: 2})).save(function (err, docUnit2) {
-                (new Palabra({letras: 'Picante', unidad: docUnit2})).save(function (err, doc) {
-                    (new Unidad({nombre: 'Bye', descripcion: 'Unidas de Saludos', nivel: 3})).save(function (err, docUnit3) {
-                        (new Palabra({letras: 'Chau', unidad: docUnit3})).save(function (err, doc) {
-                            (new Unidad({nombre: 'Carrera', descripcion: 'Unidas de Comida', nivel: 4})).save(function (err, docUnit4) {
-                                (new Palabra({letras: 'Correr', unidad: docUnit4})).save(function (err, doc) {
-                                    (new Unidad({nombre: 'Bailar', descripcion: 'Unidas de Comida', nivel: 5})).save(function (err, docUnit5) {
-                                        (new Palabra({letras: 'Conga', unidad: docUnit5})).save(function (err, doc) {
-                                            (new Unidad({nombre: 'Saludos', descripcion: 'Unidas de Saludos', nivel: 6})).save(function (err, docUnit6) {
-                                                (new Palabra({letras: 'Hola', unidad: docUnit6})).save(function (err, doc) {
-                                                    (new Unidad({nombre: 'Comida', descripcion: 'Unidas de Comida', nivel: 7})).save(function (err, docUnit7) {
-                                                        (new Palabra({letras: 'Picante', unidad: docUnit7})).save(function (err, doc) {
-                                                            (new Unidad({nombre: 'Bye', descripcion: 'Unidas de Saludos', nivel: 8})).save(function (err, docUnit8) {
-                                                                (new Palabra({letras: 'Chau', unidad: docUnit8})).save(function (err, doc) {
-                                                                    (new Unidad({nombre: 'Carrera', descripcion: 'Unidas de Comida', nivel: 9})).save(function (err, docUnit9) {
-                                                                        (new Palabra({letras: 'Correr', unidad: docUnit9})).save(function (err, doc) {
-                                                                            (new Unidad({nombre: 'Bailar', descripcion: 'Unidas de Comida', nivel: 10})).save(function (err, docUnit10) {
-                                                                                (new Palabra({letras: 'Conga', unidad: docUnit10})).save(function (err, doc) {
+                (new Palabra({letras: 'PICANTE', unidad: docUnit2})).save(function (err, doc) {
+                    (new Unidad({
+                        nombre: 'Bye',
+                        descripcion: 'Unidas de Saludos',
+                        nivel: 3
+                    })).save(function (err, docUnit3) {
+                        (new Palabra({letras: 'CHAU', unidad: docUnit3})).save(function (err, doc) {
+                            (new Unidad({
+                                nombre: 'Carrera',
+                                descripcion: 'Unidas de Comida',
+                                nivel: 4
+                            })).save(function (err, docUnit4) {
+                                (new Palabra({letras: 'CORRER', unidad: docUnit4})).save(function (err, doc) {
+                                    (new Unidad({
+                                        nombre: 'Bailar',
+                                        descripcion: 'Unidas de Comida',
+                                        nivel: 5
+                                    })).save(function (err, docUnit5) {
+                                        (new Palabra({
+                                            letras: 'TAQUIRARI',
+                                            unidad: docUnit5
+                                        })).save(function (err, doc) {
+                                            (new Unidad({
+                                                nombre: 'Saludos',
+                                                descripcion: 'Unidas de Saludos',
+                                                nivel: 6
+                                            })).save(function (err, docUnit6) {
+                                                (new Palabra({
+                                                    letras: 'HOLA',
+                                                    unidad: docUnit6
+                                                })).save(function (err, doc) {
+                                                    (new Unidad({
+                                                        nombre: 'Comida',
+                                                        descripcion: 'Unidas de Comida',
+                                                        nivel: 7
+                                                    })).save(function (err, docUnit7) {
+                                                        (new Palabra({
+                                                            letras: 'PICANTE',
+                                                            unidad: docUnit7
+                                                        })).save(function (err, doc) {
+                                                            (new Unidad({
+                                                                nombre: 'Bye',
+                                                                descripcion: 'Unidas de Saludos',
+                                                                nivel: 8
+                                                            })).save(function (err, docUnit8) {
+                                                                (new Palabra({
+                                                                    letras: 'CHAU',
+                                                                    unidad: docUnit8
+                                                                })).save(function (err, doc) {
+                                                                    (new Unidad({
+                                                                        nombre: 'Carrera',
+                                                                        descripcion: 'Unidas de Comida',
+                                                                        nivel: 9
+                                                                    })).save(function (err, docUnit9) {
+                                                                        (new Palabra({
+                                                                            letras: 'CORRER',
+                                                                            unidad: docUnit9
+                                                                        })).save(function (err, doc) {
+                                                                            (new Unidad({
+                                                                                nombre: 'Bailar',
+                                                                                descripcion: 'Unidas de Comida',
+                                                                                nivel: 10
+                                                                            })).save(function (err, docUnit10) {
+                                                                                (new Palabra({
+                                                                                    letras: 'TAQUIRARI',
+                                                                                    unidad: docUnit10
+                                                                                })).save(function (err, doc) {
                                                                                     mongoose.disconnect();
                                                                                 });
                                                                             });
@@ -92,9 +140,9 @@ function seedTest() {
 function seedTest2() {
     // Unidad
     (new Unidad({nombre: 'Saludos', descripcion: 'Unidas de Saludos', nivel: 1})).save(function (err, docUnit1) {
-        (new Palabra({letras: 'AAA', unidad: docUnit1})).save(function (err, doc) {
-            (new Palabra({letras: 'DDD', unidad: docUnit1})).save(function (err, doc) {
-                (new Palabra({letras: 'MMM', unidad: docUnit1})).save(function (err, doc) {
+        (new Palabra({letras: 'A', unidad: docUnit1})).save(function (err, doc) {
+            (new Palabra({letras: 'D', unidad: docUnit1})).save(function (err, doc) {
+                (new Palabra({letras: 'MM', unidad: docUnit1})).save(function (err, doc) {
                     mongoose.disconnect();
                 });
             });
