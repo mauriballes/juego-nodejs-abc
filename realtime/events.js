518675005da65095beb8f0cb944cfac1eb625507
Mauricio Ballesteros <mballes95@gmail.com>
2016-12-12 06:25:44 -0400

    Subiendo a produccion

--- realtime/events.js
+++ realtime/events.js
@@ -5,6 +5,7 @@ module.exports = function (socket) {
     var identifyGame = require('../game/identify');
     var requestPlayGame = require('../game/requestPlay');
     var cancelPlayRequestGame = require('../game/cancelPlayRequest');
+    var endPlayGame = require('../game/endPlay');
     var utils = require('./utils');
 
     // Events
@@ -12,6 +13,9 @@ module.exports = function (socket) {
     socket.on('identify', identify);
     socket.on('requestPlay', requestPlay);
     socket.on('cancelPlayRequest', cancelPlayRequest);
+    socket.on('playGame', playGame);
+    socket.on('touched', touched);
+    socket.on('endGame', endGame);
     socket.on('disconnect', disconnect);
 
     // Handlers
@@ -37,12 +41,63 @@ module.exports = function (socket) {
     function requestPlay(data) {
         requestPlayGame(data, socket);
     }
-    
+
     /**
-     * Cancelar peticion de partida 
+     * Cancelar peticion de partida
      */
     function cancelPlayRequest(data) {
         cancelPlayRequestGame(socket);
+    }
+
+    /**
+     * Metodo con el que se pasan los objetos del juego
+     */
+    function playGame(data) {
+        if (data.idPartida > -1) {
+            // TODO: Validar en caso de que el oponente este conectado
+            listPartidas[data.idPartida].player2.socket.emit('playGameRes', {
+                status: 'OK',
+                evento: 'playGameRes',
+                letras: data.letras
+            });
+        } else {
+            // TODO: Cuando un jugar mande letras para su oponente y que no exista su partida
+        }
+    }
+
+    /**
+     * Metodo en el que se mandan la letras seleccionadas al otro jugador
+     */
+    function touched(data) {
+        if (data.idPartida > -1) {
+            // TODO: Validar en caso de que el oponente este conectado
+            if (data.player === 'Creador')
+                listPartidas[data.idPartida].player2.socket.emit('touchedRes', {
+                    status: 'OK',
+                    evento: 'touchedRes',
+                    letra: data.letra
+                });
+            else
+                listPartidas[data.idPartida].player1.socket.emit('touchedRes', {
+                    status: 'OK',
+                    evento: 'touchedRes',
+                    letra: data.letra
+                });
+        } else {
+            // TODO: Cuando un jugar mande letras para su oponente y que no exista su partida
+        }
+    }
+
+    /**
+     * Metodo que da puntos y revisa por los ganadores del nivel
+     */
+    function endGame(data) {
+        if (data.idPartida > -1) {
+            // TODO: Validar en caso de que el oponente este conectado
+            endPlayGame(data, socket);
+        } else {
+            // TODO: Cuando un jugar mande letras para su oponente y que no exista su partida
+        }
     }
 
     /**
