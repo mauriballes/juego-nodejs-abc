fe5a71f2e69fe8de138432caaa371ef67d7f7843
Mauricio Ballesteros <mballes95@gmail.com>
2016-12-12 03:03:44 -0400

    Agregando nueva estructura de partida

--- realtime/models.js
+++ realtime/models.js
@@ -5,13 +5,14 @@
 function clienteConectado(socket, client, isPlaying){
     this.socket = socket || null;
     this.client = client || null;
-    this.isPlaying = isPlaying || false;
 }
 
-function partida(player1, player2, unidad) {
+function partida(player1, player2, unidad, palabras) {
     this.player1 = player1 || null;
     this.player2 = player2 || null;
     this.unidad = unidad || null;
+    this.palabras = palabras || [];
+    this.puntos = {player1: 0, player2: 0};
 }
 
 module.exports.clienteConectado = clienteConectado;
